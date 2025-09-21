447d21904b64d88f2a048476c93670580303fecb
luxuryfaisal <fai9al.luxury@gmail.com>
2025-09-21 09:54:34 +0000

    Fixed image path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,11 +13,12 @@ import OrderTracker from "@/pages/OrderTracker";
     query: "?url",
     import: "default"
   });
-  const bgLogoUrl = Object.values(bgLogos)[0] as string | undefined;
-
+  const bgLogos = import.meta.glob("../../attached_assets/1000063409_1758280754249.png", {
   eager: true,
   query: "?url",
-  import: "default",
+  import: "default"
+});
+
 });
 const bgLogoUrl = Object.values(bgLogos)[0] as string | undefined;
 
